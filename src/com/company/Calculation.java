c702f50b73872ed63b53ce98c12e6387d1bc4e69
honey <shubhamakkicool@gmail.com>
2022-04-18 16:11:39 -0400

    Creating Calculation Class For two numbers

---
+++ src/com/company/Calculation.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Calculation {
+    public static void main(String[] args) {
+        int i=10;
+        int j=20;
+        int c=i+j;
+        System.out.println("Sum of "+i+"+"+j+"="+c);
+
+    }
+}
